41a364084effe7b0c7f4cf932c323cbc2e842b74
Thiago Valadao <inl.aax@gmail.com>
2023-06-30 18:15:37 -0300

    first commit

---
+++ Matrizes/Questao1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void InverterDiagonais(int[,] matriz)
+    {
+        int tamanho = matriz.GetLength(0);
+
+        // Invertendo a diagonal principal
+        for (int i = 0; i < tamanho / 2; i++)
+        {
+            for (int j = 0; j < tamanho / 2; j++)
+            {
+                int temp = matriz[i, j];
+                matriz[i, j] = matriz[tamanho - 1 - i, tamanho - 1 - j];
+                matriz[tamanho - 1 - i, tamanho - 1 - j] = temp;
+            }
+        }
+
+        // Invertendo a diagonal secundária
+        for (int i = 0; i < tamanho / 2; i++)
+        {
+            for (int j = tamanho / 2 + 1; j < tamanho; j++)
+            {
+                int temp = matriz[i, j];
+                matriz[i, j] = matriz[tamanho - 1 - i, tamanho - 1 - j];
+                matriz[tamanho - 1 - i, tamanho - 1 - j] = temp;
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int[,] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        Console.WriteLine("Matriz original:");
+        ImprimirMatriz(matriz);
+
+        InverterDiagonais(matriz);
+
+        Console.WriteLine("\nMatriz com diagonais invertidas:");
+        ImprimirMatriz(matriz);
+    }
+
+    static void ImprimirMatriz(int[,] matriz)
+    {
+        int tamanho = matriz.GetLength(0);
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            for (int j = 0; j < tamanho; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Matrizes2/Questao2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Program
+{
+    static int ContarSubmatriz(int[,] matrizA, int[,] submatrizB)
+    {
+        int linhasA = matrizA.GetLength(0);
+        int colunasA = matrizA.GetLength(1);
+        int linhasB = submatrizB.GetLength(0);
+        int colunasB = submatrizB.GetLength(1);
+
+        int contador = 0;
+
+        for (int i = 0; i <= linhasA - linhasB; i++)
+        {
+            for (int j = 0; j <= colunasA - colunasB; j++)
+            {
+                if (VerificarSubmatriz(matrizA, submatrizB, i, j))
+                {
+                    contador++;
+                }
+            }
+        }
+
+        return contador;
+    }
+
+    static bool VerificarSubmatriz(int[,] matrizA, int[,] submatrizB, int inicioLinha, int inicioColuna)
+    {
+        int linhasB = submatrizB.GetLength(0);
+        int colunasB = submatrizB.GetLength(1);
+
+        for (int i = 0; i < linhasB; i++)
+        {
+            for (int j = 0; j < colunasB; j++)
+            {
+                if (matrizA[inicioLinha + i, inicioColuna + j] != submatrizB[i, j])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        int[,] matrizA = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 1, 2, 3},
+            {4, 5, 6, 7}
+        };
+
+        int[,] submatrizB = {
+            {1, 2},
+            {5, 6}
+        };
+
+        int ocorrencias = ContarSubmatriz(matrizA, submatrizB);
+        Console.WriteLine("Número de ocorrências da submatriz B na matriz A: " + ocorrencias);
+    }
+}
